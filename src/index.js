94c5f55694e2cc290f49ce411ff9518ba08f392a
Flaise <x.Flaise@gmail.com>
2016-05-05 17:27:00 -0500

    Serializing and deserializing primitives and structures without nesting.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,64 @@
 'use strict';
 
-module.exports.func = function(a) {
-    return a + 1;
+var defaultContext = Object.freeze({
+    '#true': true,
+    '#false': false,
+    '#null': null,
+    '#undefined': undefined,
+    '#nan': NaN,
+    '#infinity': Infinity,
+    '#-infinity': -Infinity,
+});
+
+module.exports.serialize = function serialize(data, context) {
+    if (arguments.length < 2) context = defaultContext;
+
+    for (var key in context) {
+        if (context[key] === data) return key;
+
+        if (context[key] !== context[key] && data !== data) return key;
+    }
+    if (Number.isFinite(data)) {
+        return '#' + data;
+    }
+
+    if (Array.isArray(data)) {
+        return 'list';
+    }
+    if (typeof data === 'string') {
+        return 'text\n' + data;
+    }
+    if (data && typeof data === 'object') {
+        return 'hash';
+    }
+
+    throw new Error('No context for "' + data + '".');
+};
+
+module.exports.deserialize = function deserialize(bytes, context) {
+    if (arguments.length < 2) context = defaultContext;
+
+    if (Object.hasOwnProperty.call(context, bytes)) {
+        return context[bytes];
+    }
+
+    var lines = bytes.split('\n');
+    var header = lines.shift();
+
+    if (header[0] === '#') {
+        var number = parseFloat(header.substr(1));
+        if (!isNaN(number)) return number;
+    }
+
+    if (header === 'hash') {
+        return {};
+    }
+    if (header === 'list') {
+        return [];
+    }
+    if (header === 'text') {
+        return lines.join('\n');
+    }
+
+    throw new Error('No context for "' + bytes + '".');
 };
--- test/test.js
+++ test/test.js
@@ -1,14 +1,86 @@
 'use strict';
 var assert = require('power-assert');
-var something = require('../src');
+var circlebytes = require('../src');
+var serialize = circlebytes.serialize;
+var deserialize = circlebytes.deserialize;
 
 
-suite('Something');
+var enums = [true, false, null, undefined, NaN, Infinity, -Infinity];
 
-test('func', function() {
-    assert(something.func(1) === 2);
+var primitives = enums.slice();
+for (var i = -10; i <= 10; i += 1) {
+    primitives.push(i);
+}
+primitives = primitives.concat([.25, 1.25, -.75]);
+
+var allTypes = primitives.concat([{}, [], {a: 2}, 'qwer']);
+
+var strings = ['', 'asdf', 'qwer', 'qwer\n', '"', "'", '""', '"oiuoi"', '1', '0', '-1', 'NaN', '\n',
+               '\nzxvc', 'a\nb\nc', '@', '#', '@1234', '#29', '#undefined', '#true'];
+
+
+suite('Serialization');
+
+test('Returns a string', function() {
+    allTypes.forEach(function(data) {
+        var bytes = serialize(data);
+        assert(typeof bytes === 'string', JSON.stringify(data));
+    });
 });
 
-test('func - 2', function() {
-    assert(4 === something.func(3));
+
+suite('Primitives');
+
+primitives.forEach(function(data) {
+    test(data, function() {
+        if (data !== data) {
+            var result = deserialize(serialize(data));
+            assert(result !== result);
+        } else {
+            assert(data === deserialize(serialize(data)));
+        }
+    });
+});
+
+
+suite('Structures');
+
+test('empty object', function() {
+    assert.deepEqual(deserialize(serialize({})), {});
+});
+
+test('empty array', function() {
+    assert.deepEqual(deserialize(serialize([])), []);
+});
+
+test('empty string', function() {
+    assert.deepEqual(deserialize(serialize('')), '');
+});
+
+
+suite('Strings');
+
+strings.forEach(function(data) {
+    test('"' + data + '"', function() {
+        assert(data === deserialize(serialize(data)));
+    });
+});
+
+
+suite('Custom Context');
+
+test('Serializing unhandled enum values', function() {
+    enums.forEach(function(data) {
+        assert.throws(function() {
+            serialize(data, {});
+        });
+    });
+});
+
+test('Deserializing unhandled enum values', function() {
+    enums.forEach(function(data) {
+        assert.throws(function() {
+            deserialize(serialize(data), {});
+        });
+    });
 });
